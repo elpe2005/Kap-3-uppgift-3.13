2c93d14803a5c345888833860766884326612b50
Admin <Admin@TEB127025>
2022-09-30 10:38:48 +0200

    Add project files.

---
+++ Kap 3 uppgift 3.13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Uppgift313
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du i heltal?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder >= 16 && ålder <= 19) 
+            {
+                Console.WriteLine("Du får delta i tävlingen");
+            }
+
+            else if (ålder < 16) 
+            {
+                Console.WriteLine("Du är för ung för att delta");
+            }
+
+            else
+            {
+                Console.WriteLine("Du är för gammal för att delta");
+            }
+        }  
+    }  
+}
